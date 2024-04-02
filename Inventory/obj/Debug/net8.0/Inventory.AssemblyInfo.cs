a31033461833e2ee0c886b0c6ad31ebd49a27164
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2024-04-02 12:18:36 -0700

    update with CICD

--- Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
+++ Inventory/obj/Debug/net8.0/Inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82291da63f052d7d09bd31c3e9e76d92a75606b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3ddb39820f160e3b8b5c15782e405f2356af46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
